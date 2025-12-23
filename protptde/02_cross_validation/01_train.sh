1de8deda9676d7e345e8e332d848d2267520e59f
Li Zheyu <67777262+lizheyu0722@users.noreply.github.com>
2025-12-23 11:46:59 +0800

    Modify the script

--- protptde/02_cross_validation/01_train.sh
+++ protptde/02_cross_validation/01_train.sh
@@ -2,69 +2,9 @@
 
 set -Eeuo pipefail
 
-# cp 2000.sh 2001.sh
-# cp 2000.sh 2002.sh
-# cp 2000.sh 2003.sh
-
-# cp 2000.sh 2010.sh
-# cp 2000.sh 2011.sh
-# cp 2000.sh 2012.sh
-# cp 2000.sh 2013.sh
-
-# cp 2000.sh 2020.sh
-# cp 2000.sh 2021.sh
-# cp 2000.sh 2022.sh
-# cp 2000.sh 2023.sh
-
-# cp 2000.sh 2030.sh
-# cp 2000.sh 2031.sh
-# cp 2000.sh 2032.sh
-# cp 2000.sh 2033.sh
-
-# cp 2000.sh 2040.sh
-# cp 2000.sh 2041.sh
-# cp 2000.sh 2042.sh
-# cp 2000.sh 2043.sh
-
-# cp 2000.sh 2050.sh
-# cp 2000.sh 2051.sh
-# cp 2000.sh 2052.sh
-# cp 2000.sh 2053.sh
-
-# cp 2000.sh 2060.sh
-
-
 mkdir -p log
 
-
-nohup bash 2000.sh > log/2000.log 2>&1 &
-# nohup bash 2001.sh > log/2001.log 2>&1 &
-# nohup bash 2002.sh > log/2002.log 2>&1 &
-# nohup bash 2003.sh > log/2003.log 2>&1 &
-
-# nohup bash 2010.sh > log/2010.log 2>&1 &
-# nohup bash 2011.sh > log/2011.log 2>&1 &
-# nohup bash 2012.sh > log/2012.log 2>&1 &
-# nohup bash 2013.sh > log/2013.log 2>&1 &
-
-# nohup bash 2020.sh > log/2020.log 2>&1 &
-# nohup bash 2021.sh > log/2021.log 2>&1 &
-# nohup bash 2022.sh > log/2022.log 2>&1 &
-# nohup bash 2023.sh > log/2023.log 2>&1 &
-
-# nohup bash 2030.sh > log/2030.log 2>&1 &
-# nohup bash 2031.sh > log/2031.log 2>&1 &
-# nohup bash 2032.sh > log/2032.log 2>&1 &
-# nohup bash 2033.sh > log/2033.log 2>&1 &
-
-# nohup bash 2040.sh > log/2040.log 2>&1 &
-# nohup bash 2041.sh > log/2041.log 2>&1 &
-# nohup bash 2042.sh > log/2042.log 2>&1 &
-# nohup bash 2043.sh > log/2043.log 2>&1 &
-
-# nohup bash 2050.sh > log/2050.log 2>&1 &
-# nohup bash 2051.sh > log/2051.log 2>&1 &
-# nohup bash 2052.sh > log/2052.log 2>&1 &
-# nohup bash 2053.sh > log/2053.log 2>&1 &
-
-# nohup bash 2060.sh > log/2060.log 2>&1 &
+nohup bash 1020.sh > log/1020.log 2>&1 &
+nohup bash 1021.sh > log/1021.log 2>&1 &
+nohup bash 1022.sh > log/1022.log 2>&1 &
+nohup bash 1023.sh > log/1023.log 2>&1 &
